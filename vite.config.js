5b8e1f0d229fdbc2d557c96f2fac846f634b638a
Antra1705 <antra.1705@gmail.com>
2026-04-01 01:03:25 +0530

    Fixed the error

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/AlgoRacer/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
